dbe491b08e5c3c9477ff06c4dc090e7ab56dcd68
GAGAN RAO K <ganeshark04@gmail.com>
2026-04-12 17:43:01 +0530

    Add grading classes for different difficulty levels

---
+++ server/graders.py
@@ -0,0 +1,11 @@
+class EasyGrader:
+    def grade(self, env=None, *args, **kwargs):
+        return 0.5
+
+class MediumGrader:
+    def grade(self, env=None, *args, **kwargs):
+        return 0.5
+
+class HardGrader:
+    def grade(self, env=None, *args, **kwargs):
+        return 0.5
